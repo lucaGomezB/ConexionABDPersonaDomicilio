0cfae4320779742ac53c8a58c2a313df96fcf278
lucaGomezB <gomez.luca.007@gmail.com>
2025-05-30 21:01:59 -0300

    Avanzado el conector a BD

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import config.DatabaseConnection;
+
+/**
+ *
+ * @author lucaGomezB <lucaGomezB at https://lucagomezb.github.io/Luca-Gomez/>
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
